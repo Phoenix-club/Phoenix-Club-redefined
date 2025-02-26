e5b6ca4f90d27323a1d0f593b46a705fd6ce5706
Omvpatil <omp838765@gmail.com>
2025-02-26 16:37:55 +0530

    Add src/assets to public

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -151,7 +151,7 @@ const Registration = ({ eventId, feesEvent }) => {
   // ✅ Otherwise, show the normal registration form
   if(eventData.name != null){
     return (
-      <div className="registration bg-[url('/bakchodi/image.png')] w-screen font-pixelSans justify-center items-center min-h-screen bg-bottom bg-cover max-lg:h-[150vh] bg-no-repeat max-lg:overflow-y-scroll  p-6 bg-[#FFCCBC] rounded-lg gap-10 max-lg:flex-col flex text-[#fff]">
+      <div className="registration bg-[url('/bakchodi/image.png')] w-screen font-pixelSans justify-center items-center h-full bg-bottom bg-cover max-lg:h-[150vh] bg-no-repeat max-lg:overflow-y-scroll  p-6 bg-[#FFCCBC] rounded-lg gap-10 max-lg:flex-col flex text-[#fff]">
 
         <div className='w-fit h-full backdrop-blur-sm p-5 items-center'>
         <h2 className="max-lg:text-2xl text-4xl font-bold mb-6">Event Registration : {eventData.name}</h2>
