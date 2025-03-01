0d77d8ed07936b1b7c18b6c2d70c7e4a491e23bb
Phoenix-club <phoenixclubkkw@gmail.com>
2025-03-01 14:41:42 +0530

    Update Announcements.jsx

--- src/components/Announcements.jsx
+++ src/components/Announcements.jsx
@@ -99,7 +99,7 @@ const Announcements = ({ setLoading, setEventId,setFeesEvent, setEvent_type }) =
               <p className='text-lg  max-md:text-sm pl-5'> Deadline: <span className='font-sans text-base'>{formatDateToIST(currentEvent.deadline)}</span></p>
               <p className='text-lg max-md:text-sm pl-5'>For : {currentEvent.event_type}</p>
               <p className='text-lg pl-5'>Venue: {currentEvent.venue}</p>
-              {backendDate > currentDate || event.name === "Imposter Escape"  ? 
+              {backendDate > currentDate || currentEvent.name === "Imposter Escape"  ? 
                 <Link onClick={()=> {setEventId(currentEvent.id); setFeesEvent(currentEvent.fees); setEvent_type(currentEvent.event_type) }} to={"/register"} state={{ data:{ name:currentEvent.name, event_type: currentEvent.event_type,  value:currentEvent.id } }} className='text-2xl w-fit p-5 group hover:cursor-pointer  text-[#1B9E64]'>
                   <span className='group-hover:hover:border-[#1B9E64] p-1 transition-all rounded-xl group-hover:hover:border-2'>
                   Register Now
